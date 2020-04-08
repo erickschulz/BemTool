1582821f6f485b90405ee8bdfb91107e275d30aa
ppanchal <ppanchal@student.ethz.ch>
2020-04-08 14:55:56 +0200

    implemented hard coded 2d mass matrix routine. General implementation pending.

--- galerkin_mat_test.cpp
+++ galerkin_mat_test.cpp
@@ -13,11 +13,11 @@ using namespace bemtool;
 
 int main(int argc, char* argv[]){
 
-  Real kappa = 0.5; // What does this mean?
+  Real kappa = 0; // Is this parameter necessary for the Laplace equation?
   Real kappa2 = kappa*kappa;
 
   // Loading the nodes into the geometry object? This is how to read .msh files
-  Geometry node("mesh/quarterunitquarebdy.msh");
+  Geometry node("mesh/circle05.msh");
   // Mesh1D object mesh? loading the node into the mesh?
   Mesh1D mesh; mesh.Load(node,1);
   // Orienting the mesh
@@ -26,36 +26,22 @@ int main(int argc, char* argv[]){
   int nb_elt = NbElt(mesh);
   std::cout << "nb_elt:\t" << nb_elt << std::endl;
 
-  // Defining the operator type (Laplacian double layer 2d)
-  // P1XP1 most probably means trial and test spaces as seen in a few lines
+  // Defining the operators, this is the right way! (test,trial)
   typedef LA_SL_2D_P0xP0 Voperator;
-  //typedef LA_DL_2D_P0xP1 Koperator;
-  //typedef CST_1D_P0xP1 Moperator;
+  typedef LA_DL_2D_P0xP1 Koperator;
 
+  using Mass_2D_P0xP1 = LocalMatrix<P0_1D,P1_1D>;
 
-  typedef LA_DL_2D_P1xP0 Koperator;
-  typedef CST_1D_P1xP0 Moperator;
+  //typedef CST_1D_P0xP1 Moperator; // CST is for double integrals!
 
-
-
-  // Defining the BIO based on the operator type
-  //BIOp<OperatorType> V(mesh,mesh,kappa);
-
-  // Defining the BIOp objects based on the operators
+  // Defining the BIOp objects based on the operators defined above
   BIOp<Voperator> VV(mesh,mesh,kappa);
   BIOp<Koperator> KK(mesh,mesh,kappa);
-  BIOp<Moperator> MM(mesh,mesh,kappa);
+  Mass_2D_P0xP1 MM(mesh);
 
-  // Dof object. Templated on the BEM space.
-  //Dof<P1_1D> dof(mesh);
-
-  // Dof objects for trial and test spaces.
+  // Dof objects for P1 and P0 (provided as template arguments)
   Dof<P1_1D> dof1(mesh);
   Dof<P0_1D> dof0(mesh);
-
-  // Degrees of freedom. The BEM space info must have been used by now to get this value.
-  //int nb_dof = NbDof(dof);
-  //std::cout << "nb_dof:\t" << nb_dof << std::endl;
 
   // Checking degrees of freedom
   int nb_dof1 = NbDof(dof1);
@@ -63,45 +49,15 @@ int main(int argc, char* argv[]){
   std::cout << "dof1 : " << nb_dof1 << std::endl;
   std::cout << "dof0 : " << nb_dof0 << std::endl;
 
-  // Initializing the Galerkin matrix A for the BIOp? A is most probably the Galerkin matrix
-  //EigenDense  A(nb_dof,nb_dof); Clear(A);
-
-  // Initializing the Galerkin matrices
   EigenDense V(nb_dof0,nb_dof0); Clear(V);
   EigenDense K(nb_dof0,nb_dof1); Clear(K); // test, trial
   EigenDense M(nb_dof0,nb_dof1); Clear(M);
 
-  /*const int n = 1;
-  // Vector of complex numbers. Size = degrees of freedom. What does it represent?
-  std::vector<Cplx> En(nb_dof);
-  // Looping over the number of elements to fill En vector
-  for(int j=0; j<nb_elt; j++){
-    const N2&         jdof = dof[j];
-    const array<2,R3> xdof = dof(j);
-    for(int k=0; k<2; k++){
-      En[jdof[k]] = pow( xdof[k][0]+iu*xdof[k][1], n);}
-  }*/
+  // Non-panel oriented assembly ---------->
 
+  // Initializing the Galerkin matrices with the right sizes. Matrix type = Complex?
   /*
-  progress bar("Assemblage",nb_elt);
-  for(int j=0; j<nb_elt; j++){
-    bar++;
-    for(int k=0; k<nb_elt; k++){
-      A(dof[j],dof[k]) += V(j,k);
-    }
-  }
-  */
-
-  // Assembly of the Galerkin matrix?
-  /*progress bar("Assemblage",nb_dof);
-  for(int j=0; j<nb_dof; j++){
-    bar++;
-    for(int k=0; k<nb_dof; k++){
-      A(j,k) += V(dof.ToElt(j),dof.ToElt(k)); // dof to elt represents global to local map?
-    }
-  }
-  bar.end();*/
-
+  // Assembly of the Galerkin matrices
   progress bar("Assemblage V",nb_dof0);
   for(int j=0; j<nb_dof0; j++){
     bar++;
@@ -115,8 +71,7 @@ int main(int argc, char* argv[]){
   for(int j=0; j<nb_dof0; j++){
     bar++;
     for(int k=0; k<nb_dof1; k++){
-        //K(j,k) += KK(dof0.ToElt(j),dof1.ToElt(k));
-        K(j,k) += KK(dof1.ToElt(j),dof0.ToElt(k));
+        K(j,k) += KK(dof0.ToElt(j),dof1.ToElt(k));
   }
 }
   bar1.end();
@@ -125,33 +80,43 @@ int main(int argc, char* argv[]){
   for(int j=0; j<nb_dof0; j++){
     bar++;
     for(int k=0; k<nb_dof1; k++){
-      //M(j,k) += MM(dof0.ToElt(j),dof1.ToElt(k));
-      M(j,k) += MM(dof1.ToElt(j),dof0.ToElt(k));
+      M(j,k) += MM(dof0.ToElt(j),dof1.ToElt(k));
     }
   }
-  bar2.end();
+  bar2.end();*/
 
-  // What is this sum for? It represents the 'solution' as it is used to determine the error.
-  /*Cplx sum =0.;
-  for(int j=0; j<nb_dof; j++){
-    for(int k=0; k<nb_dof; k++){
-      sum+= A(j,k)*conj(En[j])*En[k]; // conj(En)^T A En
+  // Panel-oriented assembly -------->
+  progress bar("Assemblage V",nb_elt);
+  for(int j=0; j<nb_elt; j++){
+    bar++;
+    for(int k=0; k<nb_elt; k++){
+      V(dof0[j],dof0[k]) += VV(j,k);
     }
   }
 
-  //  Cplx refsol = RefSol<OperatorType>::Compute(n,1.,kappa);
-  Cplx refsol = RefEigenvalue<OperatorType>::Compute(n,1.,kappa);
-  std::cout << "erreur relative:\t";
-  //std::cout << abs(sum) << " %" << std::endl;
-  std::cout << 100*sqrt( abs(sum - refsol)/abs(refsol) )<< " %" << std::endl;*/
+  progress bar1("Assemblage K",nb_elt);
+  for(int j=0; j<nb_elt; j++){
+    bar1++;
+    for(int k=0; k<nb_elt; k++){
+      K(dof0[j],dof1[k]) += KK(j,k);
+    }
+  }
 
-  Eigen::VectorXd g_N = Eigen::VectorXd::Constant(nb_dof1,1);
+  progress bar2("Assemblage M",nb_elt);
+  for(int j=0; j<nb_elt; j++){
+    bar2++;
+      M(dof0[j],dof1[j]) += MM(j);
+  }
 
+  // Vector encoding the Dirichlet boundary condition
+  Eigen::VectorXd g = Eigen::VectorXd::Constant(nb_dof1,5);
+
+  // Galerkin matrices of double type.
   Eigen::MatrixXd V1(nb_dof0,nb_dof0);
   Eigen::MatrixXd K1(nb_dof0,nb_dof1);
   Eigen::MatrixXd M1(nb_dof0,nb_dof1);
 
-  // Need the vector representing the dirichlet boundary condition!
+  // Extracting the real parts of the Galerkin matrices to solve the linear system
   for (int i = 0 ; i < nb_dof0 ; ++i) {
     for (int j = 0 ; j < nb_dof0 ; ++j) {
       V1(i,j) = V(i,j).real();
@@ -165,10 +130,9 @@ int main(int argc, char* argv[]){
     }
   }
 
-  //Eigen::MatrixXd test = V.Real();
-
-
-  Eigen::VectorXd Neumann_trace = V1.lu().solve((0.5*M1+K1)*g_N);
-  std::cout << "Neumann trace evaluated \n" << Neumann_trace << std::endl;
+  // Solving the linear system given by direct first kind formulation
+  Eigen::VectorXd Neumann_trace = V1.lu().solve((0.5*M1+K1)*g);
+  std::cout << "Neumann trace evaluated \n" << Neumann_trace.transpose() << std::endl;
+  std::cout << "RHS (expected to be 0 vector): \n" << ((0.5*M1+K1)*g).transpose() << std::endl;
 
 }
